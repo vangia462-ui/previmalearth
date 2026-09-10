d8d8a34ff402a87d5779fc1f01939488caecf81a
vangia462-ui <vangia462@gmail.com>
2026-09-10 11:28:02 +0700

    Remove comments and instructions from script
    Removed script header and usage instructions from privemalearthv2.lua.

--- privemalearthv2.lua
+++ privemalearthv2.lua
@@ -1,15 +1,1 @@
-# Primeval Earth Mobile Script
-
-Script Auto Farm + ESP cho game Primeval Earth (Roblox).
-Tối ưu cho Android Executor (Arceus X, Hydrogen, Delta, Codex).
-
-## Tính năng
-- Auto Eat / Drink / Rest / Zone
-- Auto Farm Fossil
-- Fly Mode (bay lơ lửng)
-- ESP: Players, Fossils, Trees, Water
-- GUI đẹp, kéo thả, phím tắt
-
-## Cách dùng
-```lua
-loadstring(game:HttpGet("https://raw.githubusercontent.com/USER/primeval-earth-script/main/PrimevalEarth.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vangia462-ui/previmalearth/main/privimalearthev2.lua"))()
